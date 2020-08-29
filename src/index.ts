06e839bd3567b40c4a8a59cb9e106d61f2a71532
Artik <mail@artik.me>
2020-08-29 18:55:19 +0300

    ADD: transforms article

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import './index.less';
-import { runCode } from './lib/runCode';
-import { highlight } from './lib/highlight';
-import { initYoutube } from './lib/initYouTube';
-import { initCodePen } from './lib/initCodePen';
-import { tableOfContents } from './lib/tableOfContents';
-import { initInternalCode } from './lib/initInternalCode';
-import { simpleVirtualScroll } from './lib/simpleVirtualScroll';
+import {runCode} from './lib/runCode';
+import {highlight} from './lib/highlight';
+import {initYoutube} from './lib/initYouTube';
+import {initCodePen} from './lib/initCodePen';
+import {tableOfContents} from './lib/tableOfContents';
+import {initInternalCode} from './lib/initInternalCode';
+import {simpleVirtualScroll} from './lib/simpleVirtualScroll';
 
 document.addEventListener('DOMContentLoaded', () => {
   const imports = [
@@ -27,16 +27,25 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-(()=>{
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   // @ts-ignore
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-   // @ts-ignore
-   ym(56975518, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
+(() => {
+  try {
+    (function (m, e, t, r, i, k, a) {
+      m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments)
+      };
+      // @ts-ignore
+      m[i].l = 1 * new Date();
+      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    // @ts-ignore
+    ym(56975518, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  } catch (e) {
+    console.warn('Yandex.Metrika is blocked')
+  }
 })()
