0cdf871f9a50fae57d782b708593cd79db28c667
Artik <amangasaryan@diasoft.ru>
2021-09-21 16:32:30 +0300

    UPD: tsconfig.json

--- src/scripts.ts
+++ src/scripts.ts
@@ -17,11 +17,11 @@ if (location.pathname.indexOf('/articles') === 0) {
   articlesObserver(entries => {
     entries.forEach(entry => {
       const article: HTMLElement = entry.target as HTMLElement;
-      const activeMenuItem = mainMenu.querySelector(`[href$="${ article.id }"]`);
+      const activeMenuItem = mainMenu?.querySelector(`[href$="${ article.id }"]`);
       if (entry.intersectionRatio > 0) {
         activeMenuItem?.classList.add('active');
         if (document.location.hash !== '#' + article.id) {
-          history.pushState(null, null, `${ location.pathname }#${ article.id }`);
+          history.pushState(null, '', `${ location.pathname }#${ article.id }`);
         }
       } else {
         activeMenuItem?.classList.remove('active');
@@ -53,11 +53,14 @@ document.addEventListener('DOMContentLoaded', () => {
   if (location.hostname !== 'localhost') {
     try {
       (function (m, e, t, r, i, k, a) {
+        // @ts-ignore
         m[i] = m[i] || function () {
+          // @ts-ignore
           (m[i].a = m[i].a || []).push(arguments)
         };
         // @ts-ignore
         m[i].l = 1 * new Date();
+        // @ts-ignore
         k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
       })
       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
